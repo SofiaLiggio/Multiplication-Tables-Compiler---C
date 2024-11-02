d10270bba4946873e7627b77f8adc770b96d896d
sofia_liggio <sofialiggio994@gmail.com>
2024-11-02 13:39:01 +0100

    create multiplication table compiler

---
+++ tabelline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int numero, tabellina, i;
+
+    numero = 0;
+
+    printf("Digita il numero di una tabellina: ");
+    scanf("%d", &tabellina);
+
+    for(i=1; i<=10; i++) {
+        printf("\n %d", numero=numero+tabellina);
+        sleep(1);
+    }
+
+    return 0;
+}
